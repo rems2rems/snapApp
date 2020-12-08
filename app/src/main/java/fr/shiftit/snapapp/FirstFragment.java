83104dcfaf8c201be5406c70e8bd15a7b107f4de
Rémy <remy.brousset@aquilenet.fr>
2020-12-08 15:16:29 +0100

    feat : demo okhttp (GET)

---
+++ app/src/main/java/fr/shiftit/snapapp/FirstFragment.java
@@ -0,0 +1,85 @@
+package fr.shiftit.snapapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView textView = (TextView)view.findViewById(R.id.textview_first);
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                OkHttpClient client = new OkHttpClient();
+
+                Request request = new Request.Builder()
+                        .url("http://10.0.2.2:3000/api/v1/users")
+                        .build();
+
+                try  {
+                    //Response response = client.newCall(request).execute();
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            e.printStackTrace();
+                            Log.e("mylogs",e.getMessage());
+                            Log.e("mylogs","exception",e);
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            String result = response.body().string();
+                            Log.i("mylogs",result);
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textView.setText(result);
+                                }
+                            });
+                            Log.i("mylogs","ok");
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e("mylogs",e.getMessage());
+                    Log.e("mylogs","exception",e);
+                }
+
+
+                //NavHostFragment.findNavController(FirstFragment.this)
+                //        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/fr/shiftit/snapapp/User.java
@@ -0,0 +1,27 @@
+package fr.shiftit.snapapp;
+
+public class User {
+
+    private String name;
+
+    private String id;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
